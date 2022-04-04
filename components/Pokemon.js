7bbd598d8d29c9c4ccf980379a68e1e9e8c5306d
rifxtt <dc.wang840533@gmail.com>
2022-04-04 14:32:27 -0400

    image

--- components/Pokemon.js
+++ components/Pokemon.js
@@ -4,7 +4,7 @@ import Image from 'next/image'
 const Pokemon = ({index, pokemon}) => {
   return (
     <div key= {index}>
-        <Image src= {`${pokemon.sprites.other["official-artwork"].front_default}`} width= {40} height= {40} layout="responsive"/>
+        <Image src= {`${pokemon.sprites.other["official-artwork"].front_default}`} width= {40} height= {40}/>
         {pokemon.name}
     </div>
   )
