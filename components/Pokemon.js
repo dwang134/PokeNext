7141037665fa813e087fba46705e347101b922b7
rifxtt <dc.wang840533@gmail.com>
2022-04-11 15:49:31 -0400

    template done

--- components/Pokemon.js
+++ components/Pokemon.js
@@ -2,10 +2,14 @@ import React from 'react'
 import Image from 'next/image'
 
 const Pokemon = ({index, pokemon}) => {
+  
+  const pokeIndex = '#'+ ('00'+ index).slice(-3);
+
   return (
-    <div key= {index}>
-        <Image src= {`${pokemon.sprites.other["official-artwork"].front_default}`} width= {40} height= {40} alt= 'pokemon'/>
-        {pokemon.name}
+    <div key= {index} className= "bg-slate-900 p-5 pt-10 rounded  flex flex-col justify-center items-center relative">
+        <div className= "absolute text-4xl top-1 right-3 text-gray-500 font-bold"> {pokeIndex}</div>
+        <Image src= {`${pokemon.sprites.other["official-artwork"].front_default}`} width= {150} height= {150} layout= 'intrinsic' alt= 'pokemon'/>
+        <div className= "capitalize text-lg tracking-wide">{pokemon.name}</div>
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,44 @@
 import Layout from '../components/Layout'
 import Pokemon from '../components/Pokemon'
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 
-export default function Home({pokemonArray}) {
+export default function Home({pokemonData}) {
 
-  const [pokemons, setPokemons]= useState(pokemonArray);
-  const [loading, setLoading]= useState(true);
+  const [pokemons, setPokemons]= useState(pokemonData);
 
-  console.log(pokemonArray[1].sprites.other["official-artwork"].front_default);
+
+  const fetchPokemon = async (URL) => {
+    
+  } 
+
 
   return (
         <Layout title= "PokéNext">
-          <div>
+          <div className= "grid xs:grid-cols-1 sm:grid-cols-3 xl:grid-cols-5 gap-10" >
             {pokemons.map((pokemon, index)=> (
-              <Pokemon key= {index} index= {index} pokemon= {pokemon}/>
+              <Pokemon key= {index} index= {index+1} pokemon= {pokemon}/>
             ))}
+          </div>
+          <div>
+              <button>prev</button>
+              <button>next</button>
           </div>
         </Layout>
   ) 
 }
 
-export async function getServerSideProps(context){
+export async function getStaticProps(context){
 
-  let pokemonArray=[];
+  let pokemonData=[];
 
   //bulb starts at 1, 897 w/o other forms
-  for (let i= 1; i< 898; i++){
+  for (let i= 1; i< 50; i++){
     const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
     const data= await res.json();
-    pokemonArray.push(data);
+    pokemonData.push(data);
   }
 
   return{
-    props: {pokemonArray}
+    props: {pokemonData}
   }
 }
